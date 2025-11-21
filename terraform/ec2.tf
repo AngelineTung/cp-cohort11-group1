d49f5d7a279dc851de0934b7d0a45445082faaee
iWilson25 <wilsonlyx250294@gmail.com>
2025-11-21 23:06:00 +0800

    initial CI/CD

---
+++ terraform/ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "app" {
+  ami           = "ami-0fa377108253bf620" # Amazon Linux 2023 (Singapore)
+  instance_type = "t2.micro"
+
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.ec2_gp1.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "capstone-gp1-ec2"
+  }
+}
