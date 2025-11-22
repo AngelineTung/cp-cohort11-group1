6606cad17ee20987d9e43ca7d02602a9bbe58690
iWilson25 <wilsonlyx250294@gmail.com>
2025-11-22 11:06:37 +0800

    remove duplicate blocks

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,18 +3,6 @@ resource "aws_instance" "app" {
   instance_type = "t2.micro"
 
   subnet_id                   = aws_subnet.public.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "project-ec2"
-  }
-}
-resource "aws_instance" "app" {
-  ami           = "ami-0889a44b331db0194"
-  instance_type = "t2.micro"
-
-  subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.ec2_gp1.id]
   associate_public_ip_address = true
 
